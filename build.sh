4c00eaff7c7b54116ccb80242fe1fb3640ba5a00
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-22 16:42:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ mkdir -p out/Library/MobileSubstrate/DynamicLibraries
 
 # 2. 拷贝 control 文件和 plist
 cp DEBIAN/control out/DEBIAN/
-cp FDFullscreenPopGesture.plist out/Library/MobileSubstrate/DynamicLibraries/
+cp Library/MobileSubstrate/DynamicLibraries/FDFullscreenPopGesture.plist out/Library/MobileSubstrate/DynamicLibraries/
 
 # 3. 编译为 dylib（假设 Xcode Command Line Tools 已安装）
 clang -dynamiclib -arch arm64 \
   -isysroot $(xcrun --sdk iphoneos --show-sdk-path) \
   -framework Foundation -framework UIKit \
-  UINavigationController+FDFullscreenPopGesture.m \
+  FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m \
   -o out/Library/MobileSubstrate/DynamicLibraries/FDFullscreenPopGesture.dylib
 
 # 4. 打包为 .deb
